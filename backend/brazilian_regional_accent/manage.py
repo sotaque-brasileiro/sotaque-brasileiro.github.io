ca9121c5284dfb0725424040bfb5127d375d5cf8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-09-24 09:33:43 -0300

    minor fix on backend

--- backend/brazilian_regional_accent/manage.py
+++ backend/brazilian_regional_accent/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brazilian_regional_accent.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brazilian_regional_accent.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
