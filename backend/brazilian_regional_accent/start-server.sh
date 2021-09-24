f66215639aac68d09f5a546a98ecade4f228af31
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-09-24 13:15:11 -0300

    minor improvements on backend and change url

--- backend/brazilian_regional_accent/start-server.sh
+++ backend/brazilian_regional_accent/start-server.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
-(cd /app; gunicorn brazilian_regional_accent.wsgi --bind 0.0.0.0:80 --workers 1 --threads 8 --timeout 0 --user www-data)
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+  (cd /app; python manage.py createsuperuser --no-input)
+fi
+(cd /app; gunicorn brazilian_regional_accent.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 8 --timeout 0 --user www-data) &
+nginx -g "daemon off;"
